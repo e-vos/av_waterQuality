0202bb050d78ba515ff24bb746dcfc5978ff0418
Elliot Vosburgh <114184064+e-vos@users.noreply.github.com>
2024-02-15 10:16:31 -0500

    Update csv_compare.py

--- Scripts/csv_compare.py
+++ Scripts/csv_compare.py
@@ -6,17 +6,22 @@ Description:
     Compare CSV files for matching cell values.
 '''
 
-import tkinter as tk
-from tkinter import filedialog
+# import tkinter as tk
+# from tkinter import filedialog
 import pandas as pd
 
 file_paths = []     # Initialize list to store selected file paths
 num_files = int(2)  # Define the number of files to be read
 
-# file1 =                       # If you have the file paths already,
-# file2 =                       # use these lines to define the paths
-# file_paths.append(file1)      # and comment out the prompt section
-# file_paths.append(file2)      # below.
+file1 = r"C:\Users\Elliot\Documents\University\Internships\AV\av_waterQuality\Datasets\CSV\RIDEM_Cyanobacteria_Advisories_thru23.csv"
+file2 = r"C:\Users\Elliot\Documents\University\Internships\AV\av_waterQuality\Datasets\CSV\WW_Test_Sites.csv"
+file_paths.append(file1)
+file_paths.append(file2)
+col1_header = 'Waterbody'
+col2_header = 'Site_DESCR'
+
+'''
+FOR FUTURE IMPLEMENTATION
 
 # Prompt the user twice to select CSV files
 
@@ -30,9 +35,12 @@ for _ in range(num_files):
     if not file_selection:
         print("No file selected. Exiting...")
     else:
-        file_paths.append(file_selection)
+        col_name = input("For the selected file, what column will be used? ")
+        
+        file_paths.append(file_selection, col_name)
         
 # print(file_paths)
+'''
 
 # Perform comparison and print uniques
 
@@ -41,11 +49,11 @@ if len(file_paths) == num_files:
     df1 = pd.read_csv(file_paths[0])
     df2 = pd.read_csv(file_paths[1])
     
-    merged_df = pd.merge(df1, df2, left_on='Waterbody', right_on='Site_DESCR', how='inner')
+    merged_df = pd.merge(df1, df2, left_on=col1_header, right_on=col2_header, how='inner')
     
-    matches_verbose = merged_df['Waterbody'].tolist()
+    matches_verbose = merged_df[col1_header].tolist()
     
-    matches_set = list(set(matches_verbose))    # No repeats
+    matches_set = sorted(list(set(matches_verbose)))    # No repeats
     
     print(matches_set)
     
@@ -54,7 +62,7 @@ if len(file_paths) == num_files:
     In the case of the RIDEM alerts and WW test sites, the following waterbodies
     have one or more overlaps:
     
-    ['Barber Pond', 'Brickyard Pond', 'Spectacle Pond', 'Larkin Pond', 'Scott Pond', 'Indian Lake', 'Almy Pond', 'Blackamore Pond', 'Warwick Pond', 'Tiogue Lake', 'Stafford Pond', 'Worden Pond', 'Wenscott Reservoir', 'Boone Lake', 'Mashapaug Pond', 'Georgiaville Pond', 'Tarkiln Pond', 'Barney Pond', 'Carbuncle Pond']
+    ['Almy Pond', 'Barber Pond', 'Barney Pond', 'Blackamore Pond', 'Boone Lake', 'Brickyard Pond', 'Carbuncle Pond', 'Georgiaville Pond', 'Indian Lake', 'Larkin Pond', 'Mashapaug Pond', 'Scott Pond', 'Spectacle Pond', 'Stafford Pond', 'Tarkiln Pond', 'Tiogue Lake', 'Warwick Pond', 'Wenscott Reservoir', 'Worden Pond']
     
     '''
 

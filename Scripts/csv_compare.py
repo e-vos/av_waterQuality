abfe59f377326959c5d57fe9f2b7f5c4f24135da
Elliot Vosburgh <114184064+e-vos@users.noreply.github.com>
2024-02-20 21:33:01 -0500

    Update csv_compare.py

--- Scripts/csv_compare.py
+++ Scripts/csv_compare.py
@@ -157,8 +157,6 @@ for filename in os.listdir(datasets_dir):
 # Note: dates in MM/DD/YYYY format in second column for all WW datasets
 #       dates in MM/DD/YYYY format in third column for filtered dataset
 
-df_alerts = pd.read_csv(r"C:\Users\Elliot\Documents\University\Internships\AV\av_waterQuality\Datasets\CSV\Filtered_Alert_WW_Stations.csv")
-
 samples_yearly = {}     # Primary dictionary used to house yearly nested dictionaries
 
 # Iterate again, like before
@@ -177,7 +175,7 @@ for filename in os.listdir(datasets_dir):
             
             samples_yearly[year] = {}
             
-        for index_row in df_samples.iterrows():
+        for index, row in df_samples.iterrows():
             
             station_id = row[0]
             sampling_date = row[1]
@@ -188,3 +186,56 @@ for filename in os.listdir(datasets_dir):
                                                                 # samples per Station ID per sample date, but filing
                                                                 # them in a dictionary constrains our data to only one
                                                                 # sample.
+
+# print(*samples_yearly)
+# Output: 2011_filtered 2012_filtered 2013_filtered 2014_filtered 2015_filtered 2016_filtered 2017_filtered 2018_filtered 2019_filtered 2020_filtered 2021_filtered
+
+df_alerts = pd.read_csv(r"C:\Users\Elliot\Documents\University\Internships\AV\av_waterQuality\Datasets\CSV\Filtered_Alert_WW_Stations.csv")
+alerts_dict = {}
+
+for index, row in df_alerts.iterrows():
+    
+    alerts_dict[row['Advisory Posted']] = row['Station_name']
+    
+# print(alerts_dict)
+
+'''
+Output:
+
+{'11/20/2023': 'WW018', '10/12/2023': 'WW150', '7/28/2023': 'WW049', '7/14/2023': 'WW016', '7/11/2023': 'WW006', '10/14/2022': 'WW016', 
+'9/2/2022': 'WW005', '8/5/2022': 'WW062', '7/22/2022': 'WW025', '6/27/2022': 'WW055', '6/10/2022': 'WW199', '10/7/2021': 'WW059', '9/30/2021': 'WW066', 
+'9/3/2021': 'WW007', '8/6/2021': 'WW062', '7/23/2021': 'WW059', '7/13/2021': 'WW049', '6/28/2021': 'WW016', '10/19/2020': 'WW062', '9/28/2020': 'WW025', 
+'9/10/2020': 'WW049', '9/4/2020': 'WW005', '8/21/2020': 'WW134', '7/16/2020': 'WW145', '7/8/2020': 'WW016', '6/24/2020': 'WW199', '8/16/2019': 'WW150', 
+'8/9/2019': 'WW025', '7/10/2019': 'WW016', '9/10/2018': 'WW049', '8/31/2018': 'WW204', '8/17/2018': 'WW016', '8/10/2018': 'WW005', '7/25/2018': 'WW025', 
+'6/27/2018': 'WW199', '9/27/2017': 'WW145', '9/12/2017': 'WW025', '8/18/2017': 'WW059', '7/31/2017': 'WW005', '7/24/2017': 'WW049', '7/21/2017': 'WW199', 
+'9/16/2016': 'WW025', '8/25/2016': 'WW059', '4/16/2016': 'WW143', '8/18/2015': 'WW059', '7/23/2015': 'WW005', '10/9/2014': 'WW005', '9/24/2014': 'WW049', 
+'7/30/2013': 'WW025', '9/14/2012': 'WW143', '9/5/2012': 'WW002', '8/28/2012': 'WW199', '8/15/2012': 'WW025', '9/23/2011': 'WW049'}
+'''
+
+matches_yearly = {}
+
+for year, year_dict in samples_yearly.items():
+    
+    matches = {key: value for key, value in alerts_dict.items() if key in year_dict and alerts_dict[key] == year_dict[key]}
+    
+    matches_yearly[year] = matches
+
+# for key, inner_dict in matches_yearly.items():
+#   
+#   print(f"{key}, {inner_dict}")
+
+'''
+Output:
+
+2011_filtered, {}
+2012_filtered, {}
+2013_filtered, {}
+2014_filtered, {}
+2015_filtered, {}
+2016_filtered, {}
+2017_filtered, {}
+2018_filtered, {}
+2019_filtered, {}
+2020_filtered, {}
+2021_filtered, {}
+'''

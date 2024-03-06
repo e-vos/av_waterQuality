80d9e7b5111e7f8066cab39c5f7bb1f0a13ce97d
Elliot Vosburgh <114184064+e-vos@users.noreply.github.com>
2024-03-05 21:36:39 -0500

    Create earthdata.py

---
+++ Scripts/earthdata.py
@@ -0,0 +1,34 @@
+''' 
+Filename: earthdata.py
+Author: Elliot Vosburgh
+Date: 5 March 2024
+Description:
+    Find HLS data quickly by bypassing clunky website search.
+'''
+
+# Set variables
+
+i_date = '2019-01-01'
+f_date = '2024-01-01'
+cloud_cover_max = 90
+
+# URLs
+
+landsat_url = (
+    "https://search.earthdata.nasa.gov/search/granules?p=C2021957657-LPCLOUD&"
+    f"pg[0][v]=f&pg[0][qt]={i_date}%2C{i_date}&pg[0][cc][max]={cloud_cover_max}&"
+    "pg[0][gsk]=-start_date&q=hls&sp[0]=-71.52539%2C41.66843&tl=1709691525!3!!&"
+    "base=landWaterMap&lat=41.81784160090278&long=-71.81982421875"
+)
+
+sentinel2_url = (
+    "https://search.earthdata.nasa.gov/search/granules?p=C2021957295-LPCLOUD&"
+    f"pg[0][v]=f&pg[0][qt]={i_date}T00%3A00%3A00.000Z%2C{f_date}T23%3A59%3A59.999Z&"
+    f"pg[0][cc][max]={cloud_cover_max}&pg[0][gsk]=-start_date&q=hls&"
+    "sp[0]=-71.52979%2C41.66813&tl=1709692070.051!3!!&lat=40.93494071937711&"
+    "long=-71.9560546875"
+)
+
+
+print(f"Landsat URL: {landsat_url}")
+print(f"Sentinel-2 URL: {sentinel2_url}")

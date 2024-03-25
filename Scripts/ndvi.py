96a132dc14e7f997953f7913b8831c139fe19a22
Elliot Vosburgh <114184064+e-vos@users.noreply.github.com>
2024-03-25 08:34:34 -0400

    Update ndvi.py

--- Scripts/ndvi.py
+++ Scripts/ndvi.py
@@ -14,17 +14,20 @@ import rasterio
 from rasterio.crs import CRS
 import earthpy.spatial as es
 
-data_path = r"D:\University\AmericaView_HLS\WW006_test_dir"
-ndvi_path = r"D:\University\AmericaView_HLS\ndvi"
+data_path = r"D:\University\AmericaView_HLS\WW006_test_dir" # Source directory
+ndvi_path = r"D:\University\AmericaView_HLS\ndvi"			# Output directory
 
+# Not all files are in tif format; filtering steps
 tif_files = glob(os.path.join(data_path, "*.tif"))
 tif_files.sort()
 
-files_by_date = {}
+files_by_date = {}	# Storage for timestamps and filenames
 
+# Just band tifs
 band_pattern = re.compile(r"\.B\d{2}\.tif$")
 band_raster_files = [file for file in tif_files if band_pattern.search(file)]
 
+# Populating files_by_date
 for tif_file in band_raster_files:
     date_part = tif_file.split(".")[3]
     files_by_date.setdefault(date_part, []).append(tif_file)
@@ -35,7 +38,7 @@ print("Organized files by timestamp. Proceeding...")
 # band_path = r"D:\University\AmericaView_HLS\WW006_test_dir\HLS.L30.T18TYM.2022171T152651.v2.0.B07.tif"
 # with rasterio.open(band_path) as src1:
 #     nodata_value = src1.nodata
-#     print("Nodata value: ", nodata_value)
+#     print("Nodata value: ", nodata_value) # For HLS, found to be -9999
 
 # Check for incomplete bands by alerting if less than 10 exist for a given date
 # for date, files in files_by_date.items():
@@ -48,21 +51,22 @@ for date, files in files_by_date.items():
 
     output_filename = os.path.join(ndvi_path, f"{date}_NDVI.tif")
     
+    # Settings for our NDVI output raster
     profile = {
         'driver': 'GTiff',
         'width': arr_st.shape[2],
         'height': arr_st.shape[1],
         'count': 1,
         'dtype': 'float32',
-        'crs': CRS.from_epsg(32618),
+        'crs': CRS.from_epsg(32618),	# WGS1984 UTM 18N **IMPORTANT**
         'transform': meta['transform'],
         'compress': 'lzw',
         'nodata': -9999
         }
     
-
+    # Write to our output directory
     with rasterio.open(output_filename, 'w', **profile) as dst:
-        dst.write(ndvi, 1)
+        dst.write(ndvi, 1)	# '1' is the position, it's also the only band
 
     print(f"Successfully saved NDVI for {date} as {output_filename}.")
 
